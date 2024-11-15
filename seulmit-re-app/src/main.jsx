d8b46315c775357741cf98ab7fc4ebc947f8c0d7
alrang0929 <sellbell07@gmail.com>
2024-11-16 03:26:17 +0900

    환경셋팅

--- seulmit-re-app/src/main.jsx
+++ seulmit-re-app/src/main.jsx
@@ -1,6 +1,8 @@
+// seulmit-re pj: Main.jsx Router 관리
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './reset.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
